768e9fa33af1018363eb0df88f76ed12b492d545
Eduardo <dudu48leste@gmail.com>
2022-10-22 17:07:12 -0300

    Adicionar arquivos de projeto.

---
+++ Projetinho/Controllers/listaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projetinho.Controllers
+{
+    public class listaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }   
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+         public IActionResult Editar()
+        {
+            return View();
+        }
+
+         public IActionResult Apagar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Projetinho/Models/JogadorModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Projetinho.Models
+{
+    public class JogadorModel
+    {
+        //encapsulamento, Proteção de dados
+        public int Id { get; set; }
+
+        // string é texto
+        public string NomeJogador { get; set; }
+
+        //int é numero inteiro
+        public int Numero { get; set; }
+
+        public string Posicao { get; set; }
+
+
+
+    }
+}
